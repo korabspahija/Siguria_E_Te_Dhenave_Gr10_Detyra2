69c8ba38b40bd8c67505ac01541462226d4598c8
Korab <korabspahiu123@hotmail.com>
2019-06-19 12:32:02 +0200

    coauthor
    Co-Authored-By: Vigan <vigan.dika@studentet.uni-pr.edu>
    Co-Authored-By: vegim-bytyqi <vegim-bytyqi@users.noreply.github.com>

---
+++ Klienti-TCP/Klienti-TCP/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Klienti_TCP
+{
+    partial class Klienti
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbRegjistrimi = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLendaPreferuar = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNotaMesatare = new System.Windows.Forms.TextBox();
+            this.txtMbiemri = new System.Windows.Forms.TextBox();
+            this.txtEmri = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPaga = new System.Windows.Forms.Label();
+            this.lblTitulli = new System.Windows.Forms.Label();
+            this.lblEmri = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPorti = new System.Windows.Forms.TextBox();
+            this.txtMesazhi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnRuaj = new System.Windows.Forms.Button();
+            this.grbRegjistrimi.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbRegjistrimi
+            // 
+            this.grbRegjistrimi.Controls.Add(this.btnRuaj);
+            this.grbRegjistrimi.Controls.Add(this.txtPassword);
+            this.grbRegjistrimi.Controls.Add(this.label1);
+            this.grbRegjistrimi.Controls.Add(this.txtLendaPreferuar);
+            this.grbRegjistrimi.Controls.Add(this.txtUsername);
+            this.grbRegjistrimi.Controls.Add(this.txtEmail);
+            this.grbRegjistrimi.Controls.Add(this.txtNotaMesatare);
+            this.grbRegjistrimi.Controls.Add(this.txtMbiemri);
+            this.grbRegjistrimi.Controls.Add(this.txtEmri);
+            this.grbRegjistrimi.Controls.Add(this.lblPassword);
+            this.grbRegjistrimi.Controls.Add(this.lblEmail);
+            this.grbRegjistrimi.Controls.Add(this.lblPaga);
+            this.grbRegjistrimi.Controls.Add(this.lblTitulli);
+            this.grbRegjistrimi.Controls.Add(this.lblEmri);
+            this.grbRegjistrimi.Controls.Add(this.lblUsername);
+            this.grbRegjistrimi.Location = new System.Drawing.Point(29, 80);
+            this.grbRegjistrimi.Name = "grbRegjistrimi";
+            this.grbRegjistrimi.Size = new System.Drawing.Size(318, 285);
+            this.grbRegjistrimi.TabIndex = 31;
+            this.grbRegjistrimi.TabStop = false;
+            this.grbRegjistrimi.Text = "Regjistrimi i studentit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Mbiemri:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtLendaPreferuar
+            // 
+            this.txtLendaPreferuar.Location = new System.Drawing.Point(115, 191);
+            this.txtLendaPreferuar.Name = "txtLendaPreferuar";
+            this.txtLendaPreferuar.PasswordChar = '*';
+            this.txtLendaPreferuar.Size = new System.Drawing.Size(196, 20);
+            this.txtLendaPreferuar.TabIndex = 39;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(115, 153);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(196, 20);
+            this.txtUsername.TabIndex = 38;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(116, 118);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(196, 20);
+            this.txtEmail.TabIndex = 37;
+            // 
+            // txtNotaMesatare
+            // 
+            this.txtNotaMesatare.Location = new System.Drawing.Point(116, 90);
+            this.txtNotaMesatare.Name = "txtNotaMesatare";
+            this.txtNotaMesatare.Size = new System.Drawing.Size(196, 20);
+            this.txtNotaMesatare.TabIndex = 36;
+            // 
+            // txtMbiemri
+            // 
+            this.txtMbiemri.Location = new System.Drawing.Point(116, 64);
+            this.txtMbiemri.Name = "txtMbiemri";
+            this.txtMbiemri.Size = new System.Drawing.Size(196, 20);
+            this.txtMbiemri.TabIndex = 35;
+            // 
+            // txtEmri
+            // 
+            this.txtEmri.Location = new System.Drawing.Point(116, 30);
+            this.txtEmri.Name = "txtEmri";
+            this.txtEmri.Size = new System.Drawing.Size(196, 20);
+            this.txtEmri.TabIndex = 34;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(15, 226);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 33;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(15, 125);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(70, 13);
+            this.lblEmail.TabIndex = 32;
+            this.lblEmail.Text = "Email adresa:";
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPaga
+            // 
+            this.lblPaga.AutoSize = true;
+            this.lblPaga.Location = new System.Drawing.Point(14, 97);
+            this.lblPaga.Name = "lblPaga";
+            this.lblPaga.Size = new System.Drawing.Size(80, 13);
+            this.lblPaga.TabIndex = 31;
+            this.lblPaga.Text = "Nota Mesatare:";
+            this.lblPaga.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTitulli
+            // 
+            this.lblTitulli.AutoSize = true;
+            this.lblTitulli.Location = new System.Drawing.Point(13, 191);
+            this.lblTitulli.Name = "lblTitulli";
+            this.lblTitulli.Size = new System.Drawing.Size(94, 13);
+            this.lblTitulli.TabIndex = 30;
+            this.lblTitulli.Text = "Lenda e preferuar:";
+            this.lblTitulli.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEmri
+            // 
+            this.lblEmri.AutoSize = true;
+            this.lblEmri.Location = new System.Drawing.Point(15, 37);
+            this.lblEmri.Name = "lblEmri";
+            this.lblEmri.Size = new System.Drawing.Size(30, 13);
+            this.lblEmri.TabIndex = 29;
+            this.lblEmri.Text = "Emri:";
+            this.lblEmri.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(15, 160);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 28;
+            this.lblUsername.Text = "Username:";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(206, 18);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(134, 62);
+            this.btnConnect.TabIndex = 30;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Enabled = false;
+            this.txtIP.Location = new System.Drawing.Point(91, 18);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(98, 20);
+            this.txtIP.TabIndex = 29;
+            this.txtIP.Text = "127.0.0.1";
+            // 
+            // txtPorti
+            // 
+            this.txtPorti.Enabled = false;
+            this.txtPorti.Location = new System.Drawing.Point(91, 47);
+            this.txtPorti.Name = "txtPorti";
+            this.txtPorti.Size = new System.Drawing.Size(45, 20);
+            this.txtPorti.TabIndex = 26;
+            this.txtPorti.Text = "2020";
+            // 
+            // txtMesazhi
+            // 
+            this.txtMesazhi.Enabled = false;
+            this.txtMesazhi.Location = new System.Drawing.Point(47, 384);
+            this.txtMesazhi.Multiline = true;
+            this.txtMesazhi.Name = "txtMesazhi";
+            this.txtMesazhi.Size = new System.Drawing.Size(300, 68);
+            this.txtMesazhi.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Porti:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "IP Adresa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Përgjegja nga SERVERI";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(115, 223);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(196, 20);
+            this.txtPassword.TabIndex = 42;
+            // 
+            // btnRuaj
+            // 
+            this.btnRuaj.Location = new System.Drawing.Point(216, 259);
+            this.btnRuaj.Name = "btnRuaj";
+            this.btnRuaj.Size = new System.Drawing.Size(102, 26);
+            this.btnRuaj.TabIndex = 43;
+            this.btnRuaj.Text = "Ruaj";
+            this.btnRuaj.UseVisualStyleBackColor = true;
+            this.btnRuaj.Click += new System.EventHandler(this.BtnRuaj_Click);
+            // 
+            // Klienti
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 450);
+            this.Controls.Add(this.grbRegjistrimi);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtPorti);
+            this.Controls.Add(this.txtMesazhi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Klienti";
+            this.Text = "Klienti";
+            this.Load += new System.EventHandler(this.Klienti_Load);
+            this.grbRegjistrimi.ResumeLayout(false);
+            this.grbRegjistrimi.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox grbRegjistrimi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLendaPreferuar;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNotaMesatare;
+        private System.Windows.Forms.TextBox txtMbiemri;
+        private System.Windows.Forms.TextBox txtEmri;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPaga;
+        private System.Windows.Forms.Label lblTitulli;
+        private System.Windows.Forms.Label lblEmri;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPorti;
+        private System.Windows.Forms.TextBox txtMesazhi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRuaj;
+        private System.Windows.Forms.TextBox txtPassword;
+    }
+}
+
---
+++ Klienti-TCP/Klienti-TCP/Form1.cs
@@ -0,0 +1,50 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Klienti_TCP
+{
+    public partial class Klienti : Form
+    {
+        public Klienti()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpClient client;
+
+        private void Klienti_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            //Connect to server
+            client.Connect("127.0.0.1", 2020);
+        }
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            //Update message to txtStatus
+            txtMesazhi.Invoke((MethodInvoker)delegate()
+            {
+                txtMesazhi.Text += e.MessageString;
+            });
+        }
+
+        private void BtnRuaj_Click(object sender, EventArgs e)
+        {
+            client.WriteLineAndGetReply(txtEmri.Text, TimeSpan.FromSeconds(3));
+        }
+    }
+}
